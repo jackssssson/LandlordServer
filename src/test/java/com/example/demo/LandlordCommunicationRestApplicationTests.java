15474e83d8b90bd661d7fcd2f2a6bc5db1def0aa
Galiber <denis.epavlov96@gmail.com>
2018-11-07 18:37:08 +0200

    ...

--- src/test/java/com/example/demo/LandlordCommunicationRestApplicationTests.java
+++ src/test/java/com/example/demo/LandlordCommunicationRestApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import daredevil.project.ProjectApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ProjectApplication.class)
 public class LandlordCommunicationRestApplicationTests {
 
     @Test
