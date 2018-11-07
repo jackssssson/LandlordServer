04ebc8d5ddd81040047e25e76329945fb3dd4654
Galiber <denis.epavlov96@gmail.com>
2018-11-07 18:57:17 +0200

    ...

--- src/main/java/daredevil/project/ProjectApplication.java
+++ src/main/java/daredevil/project/ProjectApplication.java
@@ -4,10 +4,17 @@ import daredevil.project.models.*;
 import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
